00850dd5a50757928dc7ff7c4eae6541d1adb1f7
Dmitriy <khaitdmitriy@gmail.com>
2022-03-17 09:44:59 -0400

    commit tree d0af93c180b52b50647cfef7b51ee2cf312a9f01

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+
+const distanceFromHqInBlocks = (pickupLocation) => {
+    if (pickupLocation <= 42){
+        return 42 - pickupLocation
+    } else {
+        return pickupLocation - 42
+    }
+}
+
+function distanceFromHqInFeet(pickupLocation){
+    return distanceFromHqInBlocks(pickupLocation) * 264
+}
+
+const distanceTravelledInFeet = (start, current) => {
+    if (start <= current){
+        return (current - start) * 264
+    } else {
+        return (start - current) * 264
+    }
+
+}
+
